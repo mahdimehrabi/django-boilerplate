0efb0e9ad88df281f7945429066b69f1a037e8d4
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-01-14 02:37:54 +0330

    Added user model to admin site

--- source/project/apps/core/admin.py
+++ source/project/apps/core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from . import models
 from django.contrib.sites.models import Site
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+admin.site.register(User, UserAdmin)
+
 
 admin.site.unregister(Site)
 
