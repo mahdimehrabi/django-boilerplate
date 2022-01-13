e188acd87aa9262f978ac540560faf820ffa7610
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-01-14 02:38:19 +0330

    Register user model as auth model

--- source/project/settings/_base.py
+++ source/project/settings/_base.py
@@ -123,7 +123,7 @@ LOGGING = {
     },
 }
 
-AUTH_USER_MODEL = 'project.apps.core.models.User'
+AUTH_USER_MODEL = 'core.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
