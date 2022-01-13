da3f37c90612271889e0da432476242aab1eb6de
mahdi mehrabi <mahdimehrabi17@gmail.com>
2022-01-14 02:37:33 +0330

    Add label to core app

--- source/project/apps/core/apps.py
+++ source/project/apps/core/apps.py
@@ -6,6 +6,7 @@ from django.db.models.signals import post_migrate
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'project.apps.core'
+    label = 'core'
 
     def ready(self) -> None:
         post_migrate.connect(build_settings, sender=self)
